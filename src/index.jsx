d8d3312d4e46079ba41c5329cb4cb9d8d25b426f
Mike Axtman <mdaxtman@gmail.com>
2017-02-25 20:40:35 -0800

    add basic configuration files and global styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import Main from "./views/main";
+import "./global.css";
 
-ReactDOM.render(/* insert app here */, document.getElementById("root"));
+ReactDOM.render(<Main/>, document.getElementById("root"));
 
 if (module.hot) {
   module.hot.accept();
---
+++ src/views/main.jsx
@@ -0,0 +1,12 @@
+import React, { Component, PropTypes } from "react";
+import styles from "./main.css";
+
+export default class Main extends Component {
+  render () {
+    return (
+      <div className={styles.container}>
+        Hello
+      </div>
+    );
+  }
+}
